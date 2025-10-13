e29ac5410fb62ec6f7df56715ffa24f5e2729674
imravi49 <149200608+imravi49@users.noreply.github.com>
2025-10-13 23:31:27 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+// tailwind.config.cjs
+```js
+module.exports = {
+content: ["./index.html", "./src/**/*.{js,jsx}"],
+theme: {
+extend: {
+colors: {
+primary: '#23292E',
+accent: '#DFB3AE',
+soft: '#DDDED9',
+gold: '#E4BC62'
+},
+fontFamily: {
+elegant: ['Playfair Display', 'serif'],
+modern: ['Inter', 'sans-serif']
+}
+}
+},
+plugins: []
+}
+```
