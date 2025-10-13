44fbbafc6983c8fe65f6a2c98775f68974be67c3
imravi49 <149200608+imravi49@users.noreply.github.com>
2025-10-13 19:51:24 +0530

    Delete src/pages directory

--- src/pages/Films.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export default function Films({adminData}){
-  const vids = adminData.videos.filter(v=>v.place==='Films').map(v=>v.url);
-  return (<div className='container'><h2>Films</h2><div className='videos-grid-full'>{vids.map((v,i)=>(<div className='vid-full' key={i}><iframe src={v} title={'f'+i} frameBorder='0' allowFullScreen></iframe></div>))}</div></div>);
-}
