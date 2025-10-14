f6f93d0e6980968d65478fc9562e9a2157b200cf
imravi49 <149200608+imravi49@users.noreply.github.com>
2025-10-14 07:23:06 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  root: '.',                // project root
+  publicDir: 'public',      // tells Vite where to find index.html
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html')
+    }
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
 })
